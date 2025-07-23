ba6b89ac89e2358a5ca56c1feffa94a47b303564
Walvy666 <businesswalvy@gmail.com>
2025-07-23 22:29:07 +0700

    Update generate-key.js

--- api/generate-key.js
+++ api/generate-key.js
@@ -2,15 +2,13 @@ import { nanoid } from 'nanoid';
 import { Low } from 'lowdb';
 import { JSONFile } from 'lowdb/node';
 
-export default async function handler(req, res) {
-  const adapter = new JSONFile('db.json');
-  const db = new Low(adapter);
-  await db.read();
-  db.data ||= { keys: [] };
+const adapter = new JSONFile('db.json');
+const db = new Low(adapter);
+await db.read();
+db.data ||= { keys: [] };
 
-  if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'Only POST allowed' });
-  }
+export default async (req, res) => {
+  if (req.method !== 'POST') return res.status(405).json({ error: 'Only POST allowed' });
 
   const key = `WALVY-${nanoid(8).toUpperCase()}`;
   const timestamp = Date.now();
@@ -19,4 +17,4 @@ export default async function handler(req, res) {
   await db.write();
 
   res.status(200).json({ success: true, key });
-}
+};
